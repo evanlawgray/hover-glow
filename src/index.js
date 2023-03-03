64c06d4870ac8b342cb8f67e80e0301c7c3c7561
Evan <evanlawgray@gmail.com>
2023-03-03 00:24:31 -0800

    Remove unused code. Make text shadow effect more subtle.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-// const cards = document.querySelectorAll('.card');
-
-// for (let card of cards) {
-//     card.addEventListener('mousemove', (event) => {
-//         const { currentTarget: target } = event;
-
-//         const rect = target.getBoundingClientRect(),
-//             x = event.clientX - rect.x,
-//             y = event.clientY - rect.y;
-
-//         target.style.setProperty('--grad-x', `${x}px`);
-//         target.style.setProperty('--grad-y', `${y}px`);
-//     });
-
-//     card.addEventListener('mouseleave', (event) => {
-
-//     });
-// }
-
 const cardsContainer = document.querySelector('.cards');
 
 cardsContainer.addEventListener('mousemove', (e) => {
